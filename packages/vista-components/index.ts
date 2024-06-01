1e606bffb61bafe5ac929290d1e9f757e44f7b06
michalgolda <michal.golda@hotmail.com>
2024-06-01 17:48:32 +0200

    build: move index.ts file to src folder

--- packages/vista-components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BlogPost, type BlogPostProps } from "./src/BlogPost";
-export { default as Greeting, type GreetingProps } from "./src/Greeting";
---
+++ packages/vista-components/src/index.ts
@@ -0,0 +1,2 @@
+export { default as BlogPost, type BlogPostProps } from "./BlogPost";
+export { default as Greeting, type GreetingProps } from "./Greeting";
