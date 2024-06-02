6cbe05c7eb4217fbc9ac93d7f7cc1208653c10c6
michalgolda <michal.golda@hotmail.com>
2024-06-02 15:34:51 +0200

    refactor: exclude logic to new methods

--- packages/vista/src/vista.ts
+++ packages/vista/src/vista.ts
@@ -3,7 +3,7 @@ import { IVista } from "./vista.interface";
 import { IRenderer } from "./renderer.interface";
 import { IComponentRepository } from "./component-repository.interface";
 import { Renderer } from "./renderer";
-import { ComponentType } from "./component.interface";
+import { ComponentType, IVistaComponent } from "./component.interface";
 import { ComponentTypeError } from "./exceptions";
 
 export class Vista implements IVista {
@@ -14,63 +14,64 @@ export class Vista implements IVista {
   constructor(componentRepository: IComponentRepository) {
     this.componentRepository = componentRepository;
     this.app = new Elysia()
-      .group("/render", (app) => {
-        const availableComponents = this.componentRepository.getComponents();
+      .get("/components", () => this.availableComponentsController())
+      .group("/render", (app) => this.renderComponentControllerFactory(app));
+  }
 
-        for (let component of availableComponents) {
-          const name = component.name;
+  availableComponentsController() {
+    const availableComponents = this.componentRepository
+      .getComponents()
+      .map((component) => {
+        const name = component.name;
 
-          app.get(
-            `/${name}`,
-            ({ query: componentProps }) => {
-              let rendererResult = null;
-
-              switch (component.type) {
-                case ComponentType.STATIC:
-                  rendererResult = this.renderer.renderStatic(
-                    component,
-                    componentProps
-                  );
-                  break;
-                case ComponentType.DYNAMIC:
-                  rendererResult = this.renderer.renderDynamic(
-                    component,
-                    componentProps
-                  );
-                  break;
-                default:
-                  throw new ComponentTypeError();
-              }
-
-              return {
-                params: componentProps,
-                markup: rendererResult,
-              };
-            },
-            {
-              query: component.props,
-            }
-          );
-        }
-
-        return app;
-      })
-      .get("/components", () => {
-        const availableComponents = this.componentRepository
-          .getComponents()
-          .map((component) => {
-            const name = component.name;
-
-            return {
-              name: name,
-              endpoint: `/render/${name}`,
-              type: component.type,
-              props: component.props,
-            };
-          });
-
-        return availableComponents;
+        return {
+          name: name,
+          endpoint: `/render/${name}`,
+          type: component.type,
+          props: component.props,
+        };
       });
+
+    return availableComponents;
+  }
+
+  renderComponentController(componentProps: any, component: IVistaComponent) {
+    let rendererResult = null;
+
+    switch (component.type) {
+      case ComponentType.STATIC:
+        rendererResult = this.renderer.renderStatic(component, componentProps);
+        break;
+      case ComponentType.DYNAMIC:
+        rendererResult = this.renderer.renderDynamic(component, componentProps);
+        break;
+      default:
+        throw new ComponentTypeError();
+    }
+
+    return {
+      params: componentProps,
+      markup: rendererResult,
+    };
+  }
+
+  renderComponentControllerFactory(app: Elysia<any>): Elysia {
+    const availableComponents = this.componentRepository.getComponents();
+
+    for (let component of availableComponents) {
+      const name = component.name;
+
+      app.get(
+        `/${name}`,
+        ({ query: componentProps }) =>
+          this.renderComponentController(componentProps, component),
+        {
+          query: component.props,
+        }
+      );
+    }
+
+    return app;
   }
 
   listen(hostname: string = "localhost", port: number = 3000): void {
